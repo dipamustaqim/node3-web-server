2aa8a98a46196a7626924bd24240eadeaf7ab3ed
unknown <user@CIC-DIPA.adira.co.id>
2020-07-23 12:25:02 +0700

    remove unnecessary priting

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side is up')
-
 const form = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
